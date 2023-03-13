a0cbb640411efa5a23a82adf3fa93bd321a8fdd0
佐々木良輔 <dynamix_sasaki@sasakiryousukenoMacBook-Pro.local>
2023-03-13 14:52:11 +0900

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { gql, useQuery } from '@apollo/client'
+import './App.css'
+
+const BOOKS = gql`
+  query {
+    test {
+      title
+      author
+    }
+  }
+`
+console.log(BOOKS)
+
+function Books() {
+  const { loading, error, data } = useQuery(BOOKS)
+
+  if (loading) return <p>ロード中・・・</p>
+  if (error) return <p>エラー</p>
+
+  console.log(data)
+  return data.test.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {author} : {title}
+      </p>
+    </div>
+  ))
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h2>GraphQL Client</h2>
+      <Books />
+    </div>
+  )
+}
+
+export default App
